48f79c7efcb0b93713b7dd642f842c23ab82c604
Vincent MONTECOT <vmonteco@e2r11p10.42.fr>
2015-03-27 18:59:42 +0100

    ssh ok

--- mission02/launch.sh
+++ mission02/launch.sh
@@ -10,4 +10,4 @@ docker build -t docker/mission02:ssh .
 docker run -p 10022:42 -d docker/mission02:ssh
 
 # boot2docker ssh tunneling.
-#boot2docker ssh -L 10022:localhost:10022
+boot2docker ssh -L 10022:localhost:10022
