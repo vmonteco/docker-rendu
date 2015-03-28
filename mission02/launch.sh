dd22ac67529e396ece917a8a730745f3749220bd
Vincent MONTECOT <vmonteco@e2r7p21.42.fr>
2015-03-28 05:52:20 +0100

    launch script modified to delete ~/.ssh/known_hosts file

--- mission02/launch.sh
+++ mission02/launch.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+
+# Delete ~/.ssh/known_hosts
+rm ~/.ssh/known_hosts
+
 # Create ssh-key :
 ssh-keygen -f mykey -P ""
 
@@ -7,7 +11,7 @@ ssh-keygen -f mykey -P ""
 docker build -t docker/mission02:ssh .
 
 # Launch image with a terminal.
-# docker run -p 10022:42 -d docker/mission02:ssh
+docker run -p 10022:42 -d docker/mission02:ssh
 
 # boot2docker ssh tunneling.
-# boot2docker ssh -L 10022:localhost:10022
+boot2docker ssh -L 10022:localhost:10022
