1a2560a7018e7fc33a76246e918ed3bfbd79e1d7
Vincent MONTECOT <vmonteco@e2r7p21.42.fr>
2015-03-28 11:09:46 +0100

    mission 03 OK

--- mission03/launch.sh
+++ mission03/launch.sh
@@ -1,35 +1,27 @@
 #!/bin/sh
 
 # more infos on https://gist.github.com/codeinthehole/7ea69f8a21c67cc07293
-
+# https://github.com/boot2docker/boot2docker/issues/678
 
 # stop boot2docker
 boot2docker stop
 
+# creating share folder.
 mkdir ~/share
 
-# mount shared folder
-VBoxManage sharedfolder add boot2docker-vm --name /share --hostpath ~/share
-# relaunch boot2docker :
-boot2docker up
+# mount shared folder while relaunching boot2docker :
+boot2docker --vbox-share="/nfs/zfs-student-5/users/2013_paris/vmonteco/share=share:/Users=users" up
+boot2docker ssh "sudo mkdir /share; sudo mount -t vboxsf share /share"
+boot2docker ssh "sudo mkdir /Users; sudo mount -t vboxsf users /Users"
 
 # Pulling archlinux image :
 docker pull base/archlinux
 
-# volume to mount to VM
-mkdir ~/volume
-
-# mounting this volume :
-boot2docker ssh "sudo mount -t vboxsf -o uid=1000,gid=50 /nfs/zfs-student-5/users/2013_paris/vmonteco/share /share"
-# "tce-load -wi sshfs-fuse ; mkdir /volume ; sudo sshfs vmonteco@localhost:/volume /nfs/zfs-student-5/users/2013_paris/vmonteco/volume"
-
-
-# Volume tou mount to container :
-mkdir ~/volume/test
-
+# building
 docker build -t volumes .
 
 # running the container :
-docker run  -ti --rm -v $(pwd)/volume/test:/volume --name myvolume volumes sh
-#docker run --rm -v $(pwd)/volume:/volume volumes myvolume
-#docker run --rm -ti -v /Users$(pwd)/volume:/volume volumes sh
+docker run  -ti --rm -v /share/test:/share -v /Users:/Users base/archlinux sh
+
+# umount and remove dir
+boot2docker ssh "sudo umount /share; sudo rmdir /share"
