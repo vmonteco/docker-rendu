7c3fc6a7a3bed35222877b657455eb0e1f8f04dd
Vincent MONTECOT <vmonteco@e2r7p21.42.fr>
2015-03-28 09:33:40 +0100

    mounting script has to be tested

--- mission03/launch.sh
+++ mission03/launch.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 
-# Launch boot2docker :
+# more infos on https://gist.github.com/codeinthehole/7ea69f8a21c67cc07293
+
+
+# stop boot2docker
+boot2docker stop
+
+mkdir ~/share
+
+# mount shared folder
+VBoxManage sharedfolder add boot2docker-vm --name /share --hostpath ~/share
+# relaunch boot2docker :
 boot2docker up
 
 # Pulling archlinux image :
 docker pull base/archlinux
 
-mkdir volume
+# volume to mount to VM
+mkdir ~/volume
+
+# mounting this volume :
+boot2docker ssh "sudo mount -t vboxsf -o uid=1000,gid=50 /nfs/zfs-student-5/users/2013_paris/vmonteco/share /share"
+# "tce-load -wi sshfs-fuse ; mkdir /volume ; sudo sshfs vmonteco@localhost:/volume /nfs/zfs-student-5/users/2013_paris/vmonteco/volume"
+
+
+# Volume tou mount to container :
+mkdir ~/volume/test
 
 docker build -t volumes .
 
 # running the container :
-docker run --rm -ti -v $(pwd)/volume:/volume volumes sh
+docker run  -ti --rm -v $(pwd)/volume/test:/volume --name myvolume volumes sh
+#docker run --rm -v $(pwd)/volume:/volume volumes myvolume
+#docker run --rm -ti -v /Users$(pwd)/volume:/volume volumes sh
