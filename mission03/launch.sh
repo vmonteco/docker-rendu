442e5258efea88644591bf8b795d56bb4990da3d
Vincent MONTECOT <vmonteco@e2r8p11.42.fr>
2015-08-07 12:08:25 +0200

    mission03 seems ok, the VM was the problem

--- mission03/launch.sh
+++ mission03/launch.sh
@@ -3,22 +3,23 @@
 # more infos on https://gist.github.com/codeinthehole/7ea69f8a21c67cc07293
 # https://github.com/boot2docker/boot2docker/issues/678
 
+set -x
+
+SHAREDIR=$HOME/share
+
 # stop boot2docker
 boot2docker stop
 
 # creating share folder.
-mkdir ~/share
+mkdir $SHAREDIR
 
 # mount shared folder while relaunching boot2docker :
-boot2docker --vbox-share="/nfs/zfs-student-5/users/2013_paris/vmonteco/share=share:/Users=users" up
+boot2docker --vbox-share="$SHAREDIR=share" up
 boot2docker ssh "sudo mkdir /share; sudo mount -t vboxsf share /share"
-boot2docker ssh "sudo mkdir /Users; sudo mount -t vboxsf users /Users"
+
 
 # Pulling archlinux image :
 docker pull base/archlinux
-
-# building
-# docker build -t volumes .
 
 # running the container :
 docker run  -ti --rm -v /share:/share -v /Users:/Users base/archlinux sh
