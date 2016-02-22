2b761b9d9f547c5bccc51c9f44349bc81499a840
Vincent MONTECOT <vmonteco@e2r10p13.42.fr>
2016-02-22 02:50:16 +0100

    script \(doesn't work\) added, how to move virtual disk

--- bonus01/script.sh
+++ bonus01/script.sh
@@ -1,5 +1,36 @@
-#!/usr/bin/sh
+#!/bin/zsh
+
+set -x
+
+MACHINE=$1
+PATH1=$2
+PATH2=$3
+DOCKER_CONF_PATH=$HOME/.docker/machine/machines/default/default/default.vbox
 
 # how to use a different volume/directory to host the boot2docker VM
-mkdir ~/.boot2docker
-echo "Dir = "$1 > ~/.boot2docker/profile
+#mkdir ~/.boot2docker
+#echo "Dir = "$1 > ~/.boot2docker/profile
+
+# stop docker machine
+#if [[ $(docker-machine status $MACHINE) == "Running" ]]; then
+#	echo "Stopping docker machine";
+#	docker-machine stop $MACHINE;
+#	echo "Docker machine stopped";
+#else
+#	echo "docker machine already down";
+#fi
+
+# Stop VirtualBox
+
+# Move virtual disk
+mv $PATH1 $PATH2
+
+# Change disk-name in VBoxConf :
+sed -i _backup 's#$PATH1#$PATH2#' $DOCKER_CONF_PATH
+
+# restart virtualbox
+#virtualbox &
+
+# restart docker-machine
+#docker-machine start $MACHINE
+#eval '$(docker-machine env $MACHINE)'
