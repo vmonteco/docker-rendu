e57fea7580b5b1c875021eda895a33b42b49e9d0
Vincent MONTECOT <vmonteco@e2r10p13.42.fr>
2016-02-22 01:08:44 +0100

    to be tested

--- mission03/launch.sh
+++ mission03/launch.sh
@@ -3,43 +3,50 @@
 # more infos on https://gist.github.com/codeinthehole/7ea69f8a21c67cc07293
 # https://github.com/boot2docker/boot2docker/issues/678
 
-set -x
+#set -x
 
 SHAREDIR=$HOME/share
-#MACHINE="default"
+MACHINE=default
+IMAGE=volume42
 
-# stop boot2docker
-#if [[ $(docker-machine status $MACHINE) == "Running" ]]; then
-#	echo "Shutting docker-machine killed."
-#	docker-machine kill $MACHINE
-#	echo "docker machine is now killed."
-#else
-#	echo "docker machine already killed."
-#fi
+# stop docker machine
+if [[ $(docker-machine status $MACHINE) == "Running" ]]; then
+	echo "Shutting docker machine down."
+	docker-machine stop $MACHINE
+	echo "Docker machine is now down."
+else
+	echo "Docker machine already down."
+fi
 
 # creating share folder.
 mkdir $SHAREDIR
 
-# mount shared folder while relaunching boot2docker :
-#echo "restarting docker machine."
-#boot2docker --vbox-share="$SHAREDIR=share" up
-#echo "docker machine restarted."
-#echo "Creating and mounting \"share\" directory in boot2docker VM."
-#boot2docker ssh "sudo mkdir /share; sudo mount -t vboxsf share /share"
-#echo "\"share\" directory created and mounted in boot2docker VM."
+# Add directory to VM :
+VBoxManage sharedfolder add $MACHINE --name share --hostpath $SHAREDIR
+
+# mount shared folder while relaunching docker machine :
+echo "restarting docker machine."
+docker-machine start $MACHINE
+echo "docker machine restarted."
+echo "Creating and mounting \"share\" directory in docker machine VM."
+docker-machine ssh $MACHINE "sudo mkdir /share; sudo mount -t vboxsf share /share"
+echo "\"share\" directory created and mounted in boot2docker VM."
 
 # Pulling archlinux image :
-#echo "Pulling base/archlinux image."
+echo "Pulling base/archlinux image."
 docker pull base/archlinux
 
+#building image :
+docker build -t $IMAGE .
+
 # running the container :
-#echo "Running base/archlinux."
-docker run  -ti --rm -v $SHAREDIR:/share -v /Users:/Users base/archlinux sh
+echo "Running base/archlinux."
+docker run  -ti --rm -v /share:/share $IMAGE sh
 
 # umount and remove dir
-#echo "Unmounting and removing shared directory from boot2docker VM."
-#docker-machine default ssh "sudo umount /share; sudo rmdir /share"
+echo "Unmounting and removing shared directory from docker-machine VM."
+docker-machine ssh $MACHINE "sudo umount /share; sudo rmdir /share"
 
-#echo "Shutting boot2docker down."
-#boot2docker down
-#echo "boot2docker is now down."
+echo "Shutting docker-machine down."
+docker-machine stop $MACHINE
+echo "docker-machine is now down."
